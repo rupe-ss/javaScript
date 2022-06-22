fca8549dc0398d1ad1b59304fd6f7c0a00b1c8b5
rupesss-09 <rupesss09@gmail.com>
2022-06-22 11:17:13 -0400

    How to escape a quotes and more ?

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ const dataTypes = [
     'boolean',
     'symbol',
 ];
+
+//4. How to skip quotation in JavaScript
+let statement = '"I am skipping a quotation"';
+console.log(statement);
+
+let statement1 = "'Single quotation'";
+console.log(statement1);
+
+//5. Escaping out other than Quote
+/*
+\'  single quote
+\"  double quote
+|| backslash
+\n newline
+Ir carriage return
+\t tab
+\b backspace
+\ f form feed
+*/
+console.log('||');
+console.log('New line should be created.\nNew line');
+console.log('Tab space is created.\t\tTab space.');
+console.log('Tab space is created.\t\bTab space.');
