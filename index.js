a6353863172907ba2bc1be87499f7873e44f84bb
rupesss-09 <rupesss09@gmail.com>
2022-06-22 10:24:08 -0400

    Covered 3 different sections introduction, different ways to write javaScript code and different types of dataTypes

---
+++ index.js
@@ -0,0 +1,14 @@
+//1. Introduction to JavaScript
+
+//2. Different ways to write JavaScript code.
+
+//3. There are 7 different datatypes in JavaScript
+const dataTypes = [
+    'undefined',
+    'null',
+    'object',
+    'number',
+    'string',
+    'boolean',
+    'symbol',
+];
