f4b7ce7ecafac3a549028c3be0e862ec450cff27
rupesss-09 <rupesss09@gmail.com>
2022-06-28 11:47:22 -0500

    Logical order in if/else if and else statements

--- function.js
+++ function.js
@@ -57,3 +57,16 @@ function elseIfStatement() {
     }
 }
 console.log(elseIfStatement());
+
+// 22. Logical order if if/else if and else statement
+function orderMyLogic(val) {
+    if (val < 5) {
+        return 'Less than 5';
+    } else if (val < 10) {
+        return 'Less than 10';
+    } else {
+        return 'Greater than or equal to 10';
+    }
+}
+// Change this value to test
+console.log(orderMyLogic(4));
