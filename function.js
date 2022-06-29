c5be12c8d67a6039abd0f6d004efaf87afa0b826
rupesss-09 <rupesss09@gmail.com>
2022-06-29 15:29:05 -0500

    Returning early pattern from functions

--- function.js
+++ function.js
@@ -150,3 +150,13 @@ function booleanValue(a, b) {
     return a < b;
 }
 console.log(booleanValue(10, 5));
+
+// 28. Returning early pattern from Functions
+function earlyPattern(a, b) {
+    if (a < 0 || b < 0) {
+        return undefined;
+    }
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+console.log(earlyPattern(3, 6));
