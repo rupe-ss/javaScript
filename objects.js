1195479931d205f8797d0cb4eba2eb87cbd165a9
rupesss-09 <rupesss09@gmail.com>
2022-06-29 21:49:54 -0500

    Testing if object has the property(hasOwnProperty)

--- objects.js
+++ objects.js
@@ -26,3 +26,18 @@ var testObj = {
 };
 console.log(testObj['an entree']);
 console.log(testObj['the drink']);
+
+// 33. Testing if object has the property(hasOwnProperty)
+function checkObj(checkProp) {
+    var myObj = {
+        gift: 'pony',
+        pet: 'kitten',
+        bed: 'sleigh',
+    };
+    if (myObj.hasOwnProperty(checkProp)) {
+        return myObj[checkProp];
+    } else {
+        return 'Not Found';
+    }
+}
+console.log(checkObj('bed'));
