625c450e7045951a6e0bb35b80e5621596fca801
rupesss-09 <rupesss09@gmail.com>
2022-06-29 22:08:42 -0500

    Record collection challenge

--- objects.js
+++ objects.js
@@ -45,3 +45,47 @@ function checkObj(checkProp) {
     }
 }
 console.log(checkObj('bed'));
+
+// 34. Record collection challenge
+// Setup
+// Question
+/*Your function must always return the entire record collection object.
+If prop isn't tracks and value isn't an empty string, update or set that album's prop to value.
+If prop is tracks but the album doesn't have a tracks property, create an empty array and add value to it.
+If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
+If value is an empty string, delete the given prop property from the album.*/
+const recordCollection = {
+    2548: {
+        albumTitle: 'Slippery When Wet',
+        artist: 'Bon Jovi',
+        tracks: ['Let It Rock', 'You Give Love a Bad Name'],
+    },
+    2468: {
+        albumTitle: '1999',
+        artist: 'Prince',
+        tracks: ['1999', 'Little Red Corvette'],
+    },
+    1245: {
+        artist: 'Robert Palmer',
+        tracks: [],
+    },
+    5439: {
+        albumTitle: 'ABBA Gold',
+    },
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+    if (value === '') {
+        delete records[id][prop];
+    } else if (prop == 'tracks') {
+        records[id][prop] = records[id][prop] || [];
+        records[id][prop].push(value);
+    } else {
+        records[id][prop] = value;
+    }
+    return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+console.log(recordCollection);
