edca899c35ea3ce480da4d53cc8ee953d9236f9d
rupesss-09 <rupesss09@gmail.com>
2022-06-29 21:02:41 -0500

    Building javaScript objects

---
+++ objects.js
@@ -0,0 +1,8 @@
+// 30. Build javaScript Objects
+// Rules:- For double word string we have to include the single quote but for single word string we don't.
+const javaScriptObject = {
+    name: 'Brutin',
+    'Eye color': 'Blue',
+    Age: 3,
+};
+console.log(typeof javaScriptObject, javaScriptObject);
